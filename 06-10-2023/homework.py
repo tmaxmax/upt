59b503969ee7f34bbac8dfbe55ab2b7c7994ca9c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-10-06 19:06:22 +0300

    06-10-2023 Fix homework, impl generic fns

--- 06-10-2023/homework.py
+++ 06-10-2023/homework.py
@@ -3,7 +3,7 @@ def valori_distincte(a, b, c):
         print("toate argumentele sunt egale")
         return 0
     
-    if a != b != c:
+    if a != b != c != a:
         print("toate argumentele sunt distincte")
         return 3
     
@@ -17,6 +17,49 @@ def valori_distincte(a, b, c):
         print_eq(1, 3)
     
     return 1
+
+def valori_distincte_alt(a, b, c):
+    locs = list(locals().values())
+    vals = {x: [] for x in locs}
+    [vals[x].append(i + 1) for (i, x) in enumerate(locs)]
+    by_len = sorted(vals.values(), key=len)
+    out, ret = [
+        (lambda: "toate valorile sunt distincte", 3),
+        (lambda: f"argumentele {' si '.join(map(str, by_len[1]))} sunt egale", 1),
+        (lambda: "toate valorile sunt egale", 0)
+    ][len(by_len)-1]
+    print(out())
+    return ret
+
+def valori_distincte_generic(*args):
+    vals = {x: [] for x in args}
+    [vals[x].append(i + 1) for (i, x) in enumerate(args)]
+    by_len = sorted(vals.values(), key=len)
+    print([
+        "argumentele %s sunt egale" % "; ".join(
+            map(lambda l: ", ".join(map(str, l)), filter(lambda x: len(x) > 1, by_len))
+        ),
+        "toate valorile sunt distincte",
+        "toate valorile sunt egale",
+    ][int(len(by_len) == len(args) or len(by_len) == 1) + int(len(by_len) == 1)])
+    return sum(int(len(x) == 1) for x in by_len)
+
+
+print(valori_distincte(1, 2, 3))
+print(valori_distincte(2, 2, 3))
+print(valori_distincte(3, 2, 3))
+print(valori_distincte(2, 3, 3))
+print(valori_distincte(2, 2, 2))
+print()
+print(valori_distincte_alt(1, 2, 3))
+print(valori_distincte_alt(2, 2, 3))
+print(valori_distincte_alt(3, 2, 3))
+print(valori_distincte_alt(2, 3, 3))
+print(valori_distincte_alt(2, 2, 2))
+print()
+print(valori_distincte_generic(2, 3, 2, 3, 3, 5, 10, 17, 17))
+print(valori_distincte_generic(2, 2, 2, 2, 2, 2))
+print(valori_distincte_generic(2, 4, 5, 7, 9, 1))
 
 
 def mediana(a, b, c):

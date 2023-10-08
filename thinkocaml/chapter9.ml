41ec245485198343d6c8db97322b7fe667826877
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-10-08 22:34:47 +0300

    thinkocaml Solve chapter 9 exercises

--- thinkocaml/chapter9.ml
+++ thinkocaml/chapter9.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+let split_words s =
+  let r = Str.regexp "[.,:;?!\"'\r\n\t ]+" in
+  let hyph = Str.regexp "-+" in
+  Str.split r s |> List.filter (fun s -> s != "" && not (Str.string_match hyph s 0))
+;;
+
+read_line () |> split_words |> List.iter (Printf.printf "'%s' ") ;
+print_newline

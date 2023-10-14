9d30ffd139b5bb5d73862bc095d8f9dfcf87709d
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-10-14 23:26:19 +0300

    algebra Impl basic lin dep check; remove Rat

--- algebra/vectors/bin/main.ml
+++ algebra/vectors/bin/main.ml
@@ -1,5 +1,55 @@
-let () =
-  read_line () |> String.split_on_char ' '
-  |> List.filter_map Rat.of_string
-  |> List.fold_left Rat.Ops.( +/ ) Rat.zero
-  |> Printf.printf "%a\n" Rat.print
+let rec read_input () =
+  let line = try Some (read_line ()) with End_of_file -> None in
+  match line with Some text -> text ^ "\n" ^ read_input () | None -> ""
+
+let ( >> ) f g x = g (f x)
+let flip f a b = f b a
+
+let vec_re =
+  {|\(((?:-?\d+(?:\.\d*)?(?: |,|, ))*-?\d+(?:\.\d*)?)\)|}
+  |> Re.Pcre.re ~flags:[ `MULTILINE ]
+  |> Re.compile
+
+let sep_re = {|( |,|, )|} |> Re.Pcre.re |> Re.compile
+
+let parse_input s =
+  Re.all vec_re s
+  |> List.map
+       (flip Re.Group.get 1 >> Re.split sep_re >> List.map Q.of_string
+      >> Array.of_list)
+  |> Array.of_list
+
+let get_print_width m col =
+  let w = ref 0 in
+  for i = 0 to Array.length m - 1 do
+    let len = String.length (Q.to_string m.(i).(col)) in
+    if len > !w then w := len
+  done;
+  !w + 1
+;;
+
+match read_input () |> parse_input |> Vec.are_linear_dependent with
+| Error _ -> Printf.eprintf "Vectorii trebuie sa aiba acelasi ordin\n"
+| Ok None -> Printf.printf "Vectorii sunt liniar independenti\n"
+| Ok (Some eq_matrix) ->
+    Printf.printf "Vectorii sunt liniar dependenti; matricea ecuatiei este:\n";
+    let rows = Array.length eq_matrix in
+    let cols = Array.length eq_matrix.(0) in
+    for i = 0 to rows - 1 do
+      for j = 0 to cols + 1 do
+        match (i, j) with
+        | 0, 0 -> print_string "┌"
+        | _, 0 when i < rows - 1 -> print_string "│"
+        | _, 0 -> print_string "└"
+        | i, j when j >= 1 && j <= cols ->
+            Printf.printf "%*s"
+              (get_print_width eq_matrix (j - 1))
+              (Q.to_string eq_matrix.(i).(j - 1))
+        | i, j when j = cols + 1 ->
+            print_string
+              (" │ 0 "
+              ^ if i = 0 then "┐" else if i = rows - 1 then "┘" else "│")
+        | _ -> failwith "unreachable"
+      done;
+      print_newline ()
+    done
---
+++ algebra/vectors/lib/Vec.ml
@@ -0,0 +1,55 @@
+let transform_to_row_echelon m =
+  let rows = Array.length m in
+  let cols = Array.length m.(0) in
+  let rec find_pivot row pivot_col res =
+    if row = rows then res
+    else if Q.(abs m.(row).(pivot_col) > abs m.(res).(pivot_col)) then
+      find_pivot (row + 1) pivot_col row
+    else find_pivot (row + 1) pivot_col res
+  in
+  let rec eliminate_row row pivot_row coef col =
+    if col = cols then ()
+    else (
+      m.(row).(col) <- Q.(m.(row).(col) - (m.(pivot_row).(col) * coef));
+      eliminate_row row pivot_row coef (col + 1))
+  in
+  let rec row_swap i j col =
+    if col = cols then ()
+    else
+      let tmp = m.(i).(col) in
+      m.(i).(col) <- m.(j).(col);
+      m.(j).(col) <- tmp;
+      row_swap i j (col + 1)
+  in
+  let rec eliminate row pivot_row pivot_col =
+    if row = rows then ()
+    else
+      let coef = Q.(m.(row).(pivot_col) / m.(pivot_row).(pivot_col)) in
+      m.(row).(pivot_col) <- Q.zero;
+      eliminate_row row pivot_row coef (pivot_col + 1);
+      eliminate (row + 1) pivot_row pivot_col
+  in
+  let rec loop pivot_row pivot_col =
+    if pivot_row = rows || pivot_col = cols then 0
+    else
+      let pivot_val_row = find_pivot pivot_row pivot_col pivot_row in
+      if Q.equal m.(pivot_val_row).(pivot_col) Q.zero then
+        loop pivot_row (pivot_col + 1)
+      else (
+        row_swap pivot_row pivot_val_row 0;
+        eliminate (pivot_row + 1) pivot_row pivot_col;
+        1 + loop (pivot_row + 1) (pivot_col + 1))
+  in
+  loop 0 0
+
+let are_linear_dependent l =
+  if not @@ Array.for_all (fun v -> Array.length v = Array.length l.(0)) l then
+    Error (Failure "Vectors must have the same size")
+  else
+    let rows = Array.length l.(0) in
+    let cols = Array.length l in
+    let eq_matrix =
+      Array.init rows (fun i -> Array.init cols (fun j -> l.(j).(i)))
+    in
+    let num_pivots = transform_to_row_echelon eq_matrix in
+    if num_pivots = cols then Ok None else Ok (Some eq_matrix)

005b22fc91f8465722c0ac73aa69fcc4d53eb4c1
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-11-24 20:31:43 +0200

    ht Improve hasher function and seeding

--- c/hash_table/hashers.c
+++ c/hash_table/hashers.c
@@ -1,24 +1,47 @@
-#include <stddef.h>
+#include <fcntl.h>
 #include <stdint.h>
-#include <time.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "hashers.h"
+#include "xxhash/xxhash.h"
 
-size_t ht_hasher_string(const void *chars) {
-    static time_t seed = 0;
-    if (seed == 0) {
-        time(&seed);
+static int read_random_number(uint64_t *dst) {
+    int fd = open("/dev/urandom", O_RDONLY);
+    if (fd < 0) {
+        return fd;
     }
 
-    // Implements
-    // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
-    const uint8_t *data = chars;
-    size_t hash = (size_t)seed ^ 2166136261;
+    const size_t expected_read = sizeof *dst;
+    if (read(fd, dst, expected_read) != expected_read) {
+        return -1;
+    }
 
-    for (uint8_t c; (c = *data++);) {
-        hash ^= c;
-        hash *= 16777619;
+    return close(fd);
+}
+
+static uint64_t fnv1a_64(const uint8_t *input, size_t size, uint64_t seed) {
+    uint64_t hash = seed ^ 14695981039346656037UL;
+
+    for (const uint8_t *c = input; c < input + size; c++) {
+        hash ^= *c;
+        hash *= 1099511628211;
     }
 
     return hash;
+}
+
+size_t ht_hasher_string(const void *chars) {
+    static uint64_t seed = 0;
+    if (seed == 0) {
+        read_random_number(&seed);
+    }
+
+    const size_t len = strlen(chars);
+
+    if (len <= 32) {
+        return fnv1a_64(chars, len, seed);
+    }
+
+    return XXH64(chars, len, seed);
 }

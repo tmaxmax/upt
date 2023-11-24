40f32d869442682966b72de72f6b02372988816b
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-11-24 05:27:48 +0200

    c Add time seed back to hasher

--- c/hash_table/hashers.c
+++ c/hash_table/hashers.c
@@ -1,14 +1,19 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "hashers.h"
 
 size_t ht_hasher_string(const void *chars) {
+    static time_t seed = 0;
+    if (seed == 0) {
+        time(&seed);
+    }
+
     // Implements
     // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
-
     const uint8_t *data = chars;
-    size_t hash = 2166136261;
+    size_t hash = (size_t)seed ^ 2166136261;
 
     for (uint8_t c; (c = *data++);) {
         hash ^= c;

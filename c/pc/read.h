6a0f38b2a873b9a6ac33ec604d0f21769147fc54
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-12-06 20:42:11 +0200

    pc Create collection reading helper library

---
+++ c/pc/read.h
@@ -0,0 +1,73 @@
+#ifndef PC_READ_H
+#define PC_READ_H
+
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#define PC_INTERNAL_READ_NUM_VEC_DECL(type, name)                              \
+    extern int read_##name##s_eof(type *dst, size_t *count,                    \
+                                  const size_t max_count, FILE *src);          \
+    extern int read_##name##s_n(type *dst, const size_t n, FILE *src);
+
+#ifdef PC_READ_IMPLEMENTATION
+#define PC_INTERNAL_READ_NUM_VEC(type, name, format_string)                    \
+    PC_INTERNAL_READ_NUM_VEC_DECL(type, name)                                  \
+    int read_##name##s_eof(type *dst, size_t *count, const size_t max_count,   \
+                           FILE *src) {                                        \
+        int ret = 1;                                                           \
+        for (*count = 0; *count < max_count; (*count)++) {                     \
+            if ((ret = fscanf(src, "%" format_string " ", dst + *count)) !=    \
+                1) {                                                           \
+                break;                                                         \
+            }                                                                  \
+        }                                                                      \
+        return ret;                                                            \
+    }                                                                          \
+    int read_##name##s_n(type *dst, const size_t n, FILE *src) {               \
+        size_t i;                                                              \
+        return read_##name##s_eof(dst, &i, n, src);                            \
+    }
+#else
+#define PC_INTERNAL_READ_NUM_VEC(type, name, format_string)                    \
+    PC_INTERNAL_READ_NUM_VEC_DECL(type, name)
+#endif // PC_READ_IMPLEMENTATION
+
+PC_INTERNAL_READ_NUM_VEC(uint8_t, u8, SCNu8)
+PC_INTERNAL_READ_NUM_VEC(uint64_t, u64, SCNu64)
+
+extern int read_to_eof_of(void *dst, const size_t sizeof_elem, size_t *count,
+                          const size_t max_count, FILE *src,
+                          int (*read_fn)(void *elem, FILE *src));
+extern int read_n_of(void *dst, const size_t sizeof_elem, const size_t n,
+                     FILE *src, int (*read_fn)(void *elem, FILE *src));
+extern int read_scanf_ret(int ret, int expected);
+
+#ifdef PC_READ_IMPLEMENTATION
+int read_to_eof_of(void *dst, const size_t sizeof_elem, size_t *count,
+                   const size_t max_count, FILE *src,
+                   int (*read_fn)(void *elem, FILE *src)) {
+    int ret = 1;
+    for (*count = 0; *count < max_count; (*count)++) {
+        if ((ret = read_fn(dst + *count * sizeof_elem, src)) != 1) {
+            break;
+        }
+    }
+    return ret;
+}
+int read_n_of(void *dst, const size_t sizeof_elem, const size_t n, FILE *src,
+              int (*read_fn)(void *elem, FILE *src)) {
+    size_t i;
+    return read_to_eof_of(dst, sizeof_elem, &i, n, src, read_fn);
+}
+int read_scanf_ret(int ret, int expected) {
+    if (ret == expected) {
+        return 1;
+    } else if (ret == EOF) {
+        return EOF;
+    }
+    return 0;
+}
+#endif // PC_READ_IMPLEMENTATION
+
+#endif // PC_READ_H

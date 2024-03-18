aaa792b6464bc64fdac7d8126d63beafd4fec8b5
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2024-03-18 23:51:22 +0200

    tpa Make nice visualizer for arrangements

--- tpa/backtrack/arrangements.cxx
+++ tpa/backtrack/arrangements.cxx
@@ -1,14 +1,29 @@
 #include <iostream>
+#include <thread>
 
 #include "backtrack.hxx"
 #include "sets.hxx"
 
 int main(int argc, char **argv) {
-    Sets arrangements(std::stoi(argv[1]), std::stoi(argv[2]), true);
-    for (const auto &arrn : bt::backtrack(std::move(arrangements))) {
+    const auto n = std::stoi(argv[1]);
+    const auto k = std::stoi(argv[2]);
+
+    std::size_t idx_solution = 0;
+    for (const auto &arrn : bt::backtrack(Sets(n, k, true))) {
+        if (idx_solution > 0) {
+            if (idx_solution % k == 0) {
+                for (int i = 0; i < k; i++) {
+                    std::cout << "\033[F";
+                }
+            }
+            std::cout << '\n';
+        }
+        idx_solution++;
         for (auto i : arrn) {
             std::cout << i << ' ';
+            std::this_thread::sleep_for(std::chrono::microseconds{300});
         }
-        std::cout << '\n';
     }
+
+    std::cout << '\n';
 }

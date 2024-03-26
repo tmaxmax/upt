5a07146b18a7b385b91cb2e846459101a7d9e42b
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2024-03-26 23:20:12 +0200

    mac Improve and fix fpi_zero

--- mac/fpi_zero.m
+++ mac/fpi_zero.m
@@ -1,20 +1,31 @@
-function x = fpi_zero(f, x0, prec)
-    max_it = 128;
-    # By having an absolute error of an order of magnitude
+function x = fpi_zero(f, x0, prec, max_iter)
+    if ~exist('max_iter', 'var') || isempty(max_iter)
+        max_iter = 128;
+    end
+    # By having a delta between iterations of an order of magnitude
     # less than the desired number of precise digits we
     # ensure that we actually get that much precision.
     tol = 10 ^ (-prec - 1); 
 
     x = x0;
 
-    for i = 1:max_it
+    for i = 1:max_iter
         xi = f(x);
-        err = xi - x;
+        delta = xi - x;
         x = xi;
-        if mean(abs(err)) < tol
+        # We take the maximum absolute delta because
+        # 1. x may also be a vector.
+        # 2. by taking the maximum it is ensured that
+        #    all other errors are in the expected
+        #    tolerance interval.
+        if max(abs(delta)) < tol
             return
         end
     end
 
-    x = NaN
+    if isvector(x)
+        x = [];
+    else
+        x = NaN;
+    end
 end
